a077927ff7250a3b336e957735fec66f342260f1
Admin <Admin@DESKTOP-I5KHVEK.mshome.net>
2026-04-22 17:51:36 +0530

    Commit N push

--- SimpleProA11/src/test/java/testNg/OrangeClaim.java
+++ SimpleProA11/src/test/java/testNg/OrangeClaim.java
@@ -1,5 +1,8 @@
 package testNg;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import genericUtilities.BaseClass;
@@ -9,9 +12,15 @@ public class OrangeClaim extends BaseClass {
 	
 	@Test(groups = "smoke")
 	public void claim() {
-		//Creating object for orangeHRM home page
-		OrangeHomepage ohp = new OrangeHomepage(driver);
-		ohp.getclaim().click();
-		System.out.println("Execution Successfully");
+		ChromeOptions option=new ChromeOptions();
+		option.addArguments("--disable-notifications");
+		
+		WebDriver driver=new ChromeDriver(option);
+		driver.get("https://www.irctc.co.in/nget/train-search");
+		
+//		//Creating object for orangeHRM home page
+//		OrangeHomepage ohp = new OrangeHomepage(driver);
+//		ohp.getclaim().click();
+//		System.out.println("Execution Successfully");
 	}
 }
